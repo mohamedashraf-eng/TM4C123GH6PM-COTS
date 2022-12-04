0137e3615d19a64b35c56c1400404eaa27ac8d75
MohamedWx <medooka203@gmail.com>
2022-12-04 20:10:57 +0200

    Added Port Struct

--- TM4C123GH6PM_COTS/01_MCAL/01_DIO_V1/DIO_CONFIG.h
+++ TM4C123GH6PM_COTS/01_MCAL/01_DIO_V1/DIO_CONFIG.h
@@ -16,6 +16,107 @@
 ----------------------------------------------------------------------
 */
 
+/**
+ * @brief PORTx Access via BUSx
+ *        set the bus id for each port.
+ *
+ * @defgroup Config Params
+ *      @arg DIO_APB_BUS_ID
+ *      @arg DIO_AHB_BUS_ID
+ */
+#define DIO_PORTA_BUS   ( DIO_APB_BUS_ID )
+#define DIO_PORTB_BUS   ( DIO_APB_BUS_ID )
+#define DIO_PORTC_BUS   ( DIO_APB_BUS_ID )
+#define DIO_PORTD_BUS   ( DIO_APB_BUS_ID )
+#define DIO_PORTE_BUS   ( DIO_APB_BUS_ID )
+#define DIO_PORTF_BUS   ( DIO_AHB_BUS_ID )
+
+/** @degroup Port A specific configurations */
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_MODE_DIGITAL
+ *      @arg DIO_PORTA_PIN_MODE_ANALOG
+ */
+#define DIO_PORTA_PIN_MODE
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_DIR_INPUT
+ *      @arg DIO_PORTA_PIN_DIR_OUTPUT
+ */
+#define DIO_PORTA_PIN_DIR
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_STRENGTH_2MA
+ *      @arg DIO_PORTA_PIN_STRENGTH_4MA
+ *      @arg DIO_PORTA_PIN_STRENGTH_8MA
+ */
+#define DIO_PORTA_PIN_STRENGTH
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_PUR_ACTIVE
+ *      @arg DIO_PORTA_PIN_PUR_INACTIVE
+ */
+#define DIO_PORTA_PIN_PUR
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_PDR_ACTIVE
+ *      @arg DIO_PORTA_PIN_PDR_INACTIVE
+ */
+#define DIO_PORTA_PIN_PDR
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_OD_ACTIVE
+ *      @arg DIO_PORTA_PIN_OD_INACTIVE
+ */
+#define DIO_PORTA_PIN_OD
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_AF_ACTIVE
+ *      @arg DIO_PORTA_PIN_AF_INACTIVE
+ */
+#define DIO_PORTA_PIN_AF
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_INT_SENSE_FE << Falling Edge.
+ *      @arg DIO_PORTA_PIN_INT_SENSE_RE << Rising Edge.
+ *      @arg DIO_PORTA_PIN_INT_SENSE_LL << Low Level.
+ *      @arg DIO_PORTA_PIN_INT_SENSE_HL << High Level.
+ *      @arg DIO_PORTA_PIN_INT_SENSE_EB << Both Edge.
+ *      @arg DIO_PORTA_PIN_INT_SENSE_LB << Both Level.
+ */
+#define DIO_PORTA_PIN_INT_SENSE
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_INT_MASK_ACTIVE
+ *      @arg DIO_PORTA_PIN_INT_MASK_INACTIVE
+ */
+#define DIO_PORTA_PIN_INT_MASK
+/**
+ * @brief .
+ *
+ * @defgroup Config Params
+ *      @arg DIO_PORTA_PIN_INIT_DATA_LOW
+ *      @arg DIO_PORTA_PIN_INIT_DATA_HIGH
+ */
+#define DIO_PORTA_PIN_INIT_DATA
 
 
 
--- TM4C123GH6PM_COTS/01_MCAL/01_DIO_V1/DIO_PRIVATE.h
+++ TM4C123GH6PM_COTS/01_MCAL/01_DIO_V1/DIO_PRIVATE.h
@@ -16,15 +16,15 @@
 ----------------------------------------------------------------------
 */
 
-typedef struct
-{
+/** @defgroup Noraml memory address */
+#define GPIOA_BASE_ADDRESS  ( (u32) () )
+#define GPIOB_BASE_ADDRESS  ( (u32) () )
+#define GPIOC_BASE_ADDRESS  ( (u32) () )
+#define GPIOD_BASE_ADDRESS  ( (u32) () )
+#define GPIOE_BASE_ADDRESS  ( (u32) () )
+#define GPIOF_BASE_ADDRESS  ( (u32) () )
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b5e371a (DIO V1 Init Commit)
-}ST_GPIOxRegisters_t;
-
+/** @defgroup Bit-Band memory address */
 
 /*
 ----------------------------------------------------------------------
@@ -32,12 +32,63 @@ typedef struct
 ----------------------------------------------------------------------
 */
 
+/** @defgroup developr macros */
+#define DIO_PORTA_PIN_NUMS      ( (u8) (0x07U) )
+#define DIO_PORTB_PIN_NUMS      ( (u8) (0x07U) )
+#define DIO_PORTC_PIN_NUMS      ( (u8) (0x07U) )
+#define DIO_PORTD_PIN_NUMS      ( (u8) (0x07U) )
+#define DIO_PORTE_PIN_NUMS      ( (u8) (0x06U) )
+#define DIO_PORTF_PIN_NUMS      ( (u8) (0x05U) )
+
 
 /*
 ----------------------------------------------------------------------
 - Data types
 ----------------------------------------------------------------------
 */
+
+typedef u8 uC_GPIOxID_t;
+
+/**
+ * @defgroup Struct data type.
+ */
+/**
+ * @brief Struct to contain all possible configuration for
+ *        the microcontroller pin.
+ * @note packing attribute is used to reduce the padding size.
+ */
+__attribute__((packing))
+typedef struct
+{
+    uC_PinID_t Pin  : 0x03U; /** @brief Pin num         */
+    u8 mode         : 0x01U; /** @brief Analog/Digital  */
+    u8 direction    : 0x01U; /** @brief Input/Output    */
+    u8 strength     : 0x02U; /** @brief 2mA/4mA/8mA     */
+    u8 pur          : 0x01U; /** @brief Active/Inactive */
+    u8 pdr          : 0x01U; /** @brief Active/Inactive */
+    u8 od           : 0x01U; /** @brief Active/Inactive */
+    u8 af           : 0x01U; /** @brief Active/Inactive */
+    u8 int_sense    : 0x02U; /** @brief Pin Int Modes   */
+    u8 int_mask     : 0x01U; /** @brief Active/Inactive */
+    u8 int_status   : 0x01U; /** @brief Active/Inactive */
+    u8 data         : 0x01U; /** @brief High/Low        */
+}St_PinCfg_t;
+/**
+ * @brief Struct for the microcontroller porta configurations and info.
+ *
+ * @note packing attribute is used to reduce the padding size.
+ */
+__attribute__((packing))
+typedef struct
+{
+    /* Struct variables */
+    St_PinCfg_t Pin[DIO_PORTA_PIN_NUMS];
+    uC_PortID_t Port;
+    uC_GPIOxID_t ID;
+
+    /* Struct methods */
+
+}St_PORTA_Cfg_t;
 
 
 /*
